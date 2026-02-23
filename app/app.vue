ed149d43a1e139c4b940bc920fad9df7615de511
Nathay <nathay.henesy@gmail.com>
2026-02-23 14:21:44 +0700

    Dashboard

--- app/app.vue
+++ app/app.vue
@@ -1,58 +1,36 @@
 <script setup lang="ts">
-import type { NavigationMenuItem } from '@nuxt/ui'
+  import type {NavigateBar} from "@nuxt/ui";
 
-const route = useRoute()
+  const route = useRoute();
 
-const items = computed<NavigationMenuItem[]>(() => [
-  {
-    label: 'Docs',
-    to: '/docs/getting-started',
-    active: route.path.startsWith('/docs/getting-started')
-  },
-  {
-    label: 'Components',
-    to: '/docs/components',
-    active: route.path.startsWith('/docs/components')
-  },
-  {
-    label: 'Figma',
-    to: 'https://go.nuxt.com/figma-ui',
-    target: '_blank'
-  },
-  {
-    label: 'Releases',
-    to: 'https://github.com/nuxt/ui/releases',
-    target: '_blank'
-  }
-])
+  const items = computed<NavigateBar[]>(() =>[
+    {
+      label: 'Home',
+      to: '/',
+      icon: 'i-lucide-home',
+      active: route.path === '/'
+    },
+    {
+      label: 'Dashboard',
+      to: '/dashboard',
+      icon: 'i-lucide-layout-dashboard',
+      active: route.path === '/dashboard'
+    },
+    {
+      label: 'Settings',
+      to: '/settings',
+      icon: 'i-lucide-settings',
+      active: route.path === '/settings'
+    }
+  ])
 </script>
 
 <template>
-  <UApp class="w-fullscreen">
+  <UApp>
     <UHeader>
-      <template #title>
-        <Logo class="h-6 w-auto" />
-      </template>
-
       <UNavigationMenu :items="items" />
-
-      <template #right>
-        <UColorModeButton />
-
-        <UTooltip
-          text="Open on GitHub"
-          :kbds="['meta', 'G']"
-        >
-          <UButton
-            color="neutral"
-            variant="ghost"
-            to="https://github.com/nuxt/ui"
-            target="_blank"
-            icon="i-simple-icons-github"
-            aria-label="GitHub"
-          />
-        </UTooltip>
-      </template>
     </UHeader>
+    <UMain></UMain>
+    <UFooter></UFooter>
   </UApp>
 </template>
