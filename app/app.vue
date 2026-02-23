fc6462943e4c5431ebfb5cc4fd768640974053c3
Nathay <nathay.henesy@gmail.com>
2026-02-23 14:46:27 +0700

    Add Header and Footer

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { NavigateBar } from '@nuxt/ui'
+import type { NavigateFooter } from '@nuxt/ui'
 
 const route = useRoute()
 
@@ -21,16 +22,49 @@ const items = computed<NavigateBar[]>(() => [
     to: '/settings',
     icon: 'i-lucide-settings',
     active: route.path === '/settings'
+  },
+  {
+    label: 'Profile',
+    to: '/profile',
+    icon: 'i-lucide-user',
+    active: route.path === '/profile'
+  },
+  {
+    label: 'Logout',
+    to: '/logout',
+    icon: 'i-lucide-log-out',
+    active: route.path === '/logout'
+  }
+])
+
+const menus = computed<NavigateFooter[]>(() => [
+  {
+    label: 'About',
+    to: '/about'
+  },
+  {
+    label: 'Contact',
+    to: '/contact'
+  },
+  {
+    label: 'Privacy Policy',
+    to: '/privacy-policy'
+  },
+  {
+    label: 'Terms of Service',
+    to: '/terms-of-service'
   }
 ])
 </script>
 
 <template>
   <UApp>
-    <UHeader>
+    <UHeader class="text-center font-bold text-lg">
       <UNavigationMenu :items="items" />
     </UHeader>
     <UMain />
-    <UFooter />
+    <UFooter>
+      <UNavigationMenu :items="menus"/>
+    </UFooter>
   </UApp>
 </template>
